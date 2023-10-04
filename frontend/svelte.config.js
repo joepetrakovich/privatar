d6864c5bd2f949fdf7df333a78ff4dadd083c2d2
Joe <jpetrakovich@gmail.com>
2023-10-04 22:27:35 +0900

    added netlify adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ edge: false, split: false })
 	}
 };
 
